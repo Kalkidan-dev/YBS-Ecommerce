840f10ced31ee6df1ec506bea54a9c83b0326fdb
KalDev-2 <kalkidangemechudev@gmail.com>
2025-04-10 16:58:28 +0300

    Added additional default allowed local host

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
 print("ALLOWED_HOSTS =", ALLOWED_HOSTS)
 
 # Application definition
