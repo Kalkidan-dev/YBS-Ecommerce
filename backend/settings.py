8001e66211f6105405b6f722e3f3d4b14ac5af04
KalDev-2 <kalkidangemechudev@gmail.com>
2025-05-11 11:15:10 +0300

    Updated some parts

--- backend/settings.py
+++ backend/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
     'django_extensions',
+    'django.contrib.sites',
+    'faker',
     'django_filters',
     'rest_framework',
     'core',
