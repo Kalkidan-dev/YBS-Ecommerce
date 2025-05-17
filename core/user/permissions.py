7a12147934dba5fd3e4328f555b23ad2e35b634b
Kalkidan-dev <cakek433@gmail.com>
2025-05-17 07:19:44 +0300

    updated

--- core/user/permissions.py
+++ core/user/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 
+
 class IsAdminOrOwner(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated
