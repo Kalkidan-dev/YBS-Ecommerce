d87e71d8783f2848491e54ea8d352b368a4f9b51
Kalkidan-dev <cakek433@gmail.com>
2025-05-18 21:59:46 +0300

    updated

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-from core.user.views import ActivateAccountView
-
+from core.user.views import ActivateAccountView  # keep if used
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -23,11 +22,14 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls) if settings.DEBUG else path('secure-admin/', admin.site.urls),
-    
+
     path('api/user/', include('core.user.urls')),
     path('api/product/', include('core.product.urls')),
     path('api/order/', include('core.order.urls')),
     path('', include('core.urls')),
+
+    # Optional activation link (add if you use this view)
+    path('api/user/activate/<uidb64>/<token>/', ActivateAccountView.as_view(), name='activate-account'),
 
     # Swagger Docs
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='swagger-json'),
